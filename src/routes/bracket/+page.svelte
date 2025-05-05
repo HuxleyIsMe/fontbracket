62c3c334ffed3c2b7e77fe9a52d6aeb1d8f3706d
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-05-05 10:51:27 -0700

    minor refactor

--- src/routes/bracket/+page.svelte
+++ src/routes/bracket/+page.svelte
@@ -2,7 +2,6 @@
 	import { MediaQuery } from 'svelte/reactivity';
 	import Contestant from './Contestant.svelte';
 	import { getBracket } from '$lib/bracket/bracket';
-	import type { Contest } from '$lib/bracket/contest';
 
 	const atLeastMedium = new MediaQuery('width >= 48rem', true);
 
@@ -15,27 +14,27 @@
 	let pair = $derived(typeof next.value !== 'string' ? next.value : null);
 	let winner = $derived(typeof next.value === 'string' ? next.value : null);
 
-	const choose = (font: string, contest: Contest) => {
-		contest.choose(font);
+	const choose = (font: string) => {
+		pair?.choose(font);
 		next = pairsIterator.next();
 	};
 </script>
 
-{#snippet contestant(font: string, side: 'left' | 'right', pair: Contest)}
-	<Contestant code={exampleJavascript} lang="js" {side} {font} onclick={() => choose(font, pair)} />
+{#snippet contestant(font: string, side: 'left' | 'right')}
+	<Contestant code={exampleJavascript} lang="js" {side} {font} onclick={() => choose(font)} />
 {/snippet}
 
 {#if winner}
 	<p>You chose {winner}.</p>
 {:else if pair}
 	<div class="grid h-full grid-cols-1 gap-4 md:grid-cols-[1fr_auto_1fr]">
-		{@render contestant(pair.fonts[0], 'left', pair)}
+		{@render contestant(pair.fonts[0], 'left')}
 		{#if atLeastMedium.current}
 			<span class="md:vr h-full"></span>
 		{:else}
 			<hr class="hr" />
 		{/if}
-		{@render contestant(pair.fonts[1], 'right', pair)}
+		{@render contestant(pair.fonts[1], 'right')}
 	</div>
 {:else}
 	<p>Not sure how you got here, but there's no valid fonts.</p>
