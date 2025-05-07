47cd15a21a5573f19c484dbeec9a18399c5419a3
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-05-07 15:07:51 -0700

    add fonts available from fontsource

--- src/lib/fonts.svelte.ts
+++ src/lib/fonts.svelte.ts
@@ -6,7 +6,57 @@ const defaultFonts = [
 	'Iosevka',
 	'Fira Code Variable',
 	'Roboto Mono Variable',
-	'IBM Plex Mono'
+	'IBM Plex Mono',
+	'Monaspace Neon',
+	'Monaspace Argon',
+	'Monaspace Krypton',
+	'Fira Mono',
+	'Source Code Pro Variable',
+	'JetBrains Mono Variable',
+	'DM Mono',
+	'Inconsolata Variable',
+	'Ubuntu Mono',
+	'PT Mono',
+	'Space Mono',
+	'Courier Prime',
+	'Geist Mono Variable',
+	'Overpass Mono Variable',
+	'Red Hat Mono Variable',
+	'Victor Mono Variable',
+	'Azeret Mono Variable',
+	'iA Writer Mono',
+	'Noto Mono',
+	'VT323',
+	'Anonymous Pro',
+	'iA Writer Duo',
+	'Chivo Mono Variable',
+	'Sometype Mono Variable',
+	'Reddit Mono Variable',
+	'Spline Sans Mono Variable',
+	'Oxygen Mono',
+	'B612 Mono',
+	'Cousine',
+	'Martian Mono Variable',
+	'Nova Mono',
+	'Xanh Mono',
+	'Maple Mono',
+	'M PLUS 1 Code Variable',
+	'Ubuntu Sans Mono Variable',
+	'Comic Mono',
+	'Kode Mono Variable',
+	'Cutive Mono',
+	'Fragment Mono',
+	'Mononoki',
+	'Iosevka Etoile',
+	'Iosevka Aile',
+	'Syne Mono',
+	'Commit Mono',
+	'League Mono',
+	'Pitagon Sans Mono',
+	'Iosevka Curly',
+	'LXGW WenKai Mono TC',
+	'Iosevka Curly Slab',
+	'UnifontEX'
 ] as const;
 
 export type Font = (typeof defaultFonts)[number];
@@ -20,6 +70,7 @@ export const setFontStore = (fonts?: Font[], key = DEFAULT_KEY) => {
 			boolean
 		][];
 		setContext(key, new SvelteMap(storedFonts));
+		return;
 	}
 	if (!fonts) {
 		fonts = [...defaultFonts];
