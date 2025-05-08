b7056703cf06ce3a9146bf4c90c99d4788465b69
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-05-08 12:28:54 -0700

    does not work ¯\_(ツ)_/¯

---
+++ src/routes/temp/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { createBracket } from 'bracketry';
+	import type { Action } from 'svelte/action';
+
+	const bracket: Action<HTMLDivElement> = (node) => {
+		createBracket(
+			{
+				rounds: [{ name: 'Round 1' }],
+				matches: [
+					{
+						roundIndex: 0,
+						order: 0,
+						sides: [
+							{
+								title: 'Iosevka',
+								isWinner: false
+							},
+							{
+								title: 'Fira Code Variable',
+								isWinner: true
+							}
+						]
+					}
+				]
+			},
+			node
+		);
+	};
+</script>
+
+<div use:bracket></div>
