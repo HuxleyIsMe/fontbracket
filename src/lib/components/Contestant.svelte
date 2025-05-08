c1e9aa26f80cae48f1a2dbdb65bb0d4ec8314799
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-05-08 11:18:38 -0700

    add keyboard shortcuts

--- src/lib/components/Contestant.svelte
+++ src/lib/components/Contestant.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import ArrowLeft from '@lucide/svelte/icons/arrow-left';
+	import ArrowRight from '@lucide/svelte/icons/arrow-right';
 	import CodeBlock from '$lib/components/CodeBlock/CodeBlock.svelte';
 	import { colorSchemeStore } from '$lib/examples/colorscheme.svelte';
 	import { languageStore } from '$lib/examples/languages.svelte';
@@ -13,7 +15,16 @@
 	const { side, font, onclick, showFontName }: Props = $props();
 	const lang = $derived(languageStore.selected);
 	const theme = $derived(colorSchemeStore.selected);
+	const keyToListenFor = $derived(side === 'left' ? 'ArrowLeft' : 'ArrowRight');
+
+	const handleKeydown = (event: KeyboardEvent) => {
+		if (event.key === keyToListenFor) {
+			onclick();
+		}
+	};
 </script>
+
+<svelte:window onkeydown={handleKeydown} />
 
 <article class="grid h-[512px] grid-rows-[1fr_auto] gap-4 md:h-[768px]">
 	{#if showFontName}
@@ -25,6 +36,15 @@
 		class="btn {side === 'left'
 			? 'preset-outlined-primary-500 hover:preset-tonal-primary'
 			: 'preset-outlined-secondary-500 hover:preset-tonal-secondary'} place-self-center"
-		>Choose</button
 	>
+		Choose
+		<kbd class="kbd">
+			{#if side === 'left'}
+				<ArrowLeft />
+			{:else}
+				<ArrowRight />
+			{/if}
+			<span class="sr-only">{side}</span>
+		</kbd>
+	</button>
 </article>
